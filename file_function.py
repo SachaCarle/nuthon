851ed402f0e5979f2983e8714b22f79abdb14b94
SachaCarle <carle.sacha@gmail.com>
2026-07-26 02:09:02 +0200

    up

--- file_function.py
+++ file_function.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 def function(path, name=..., globals=..., locals=..., **kw):
     if not isinstance(path, Path): path = Path(path)
-    if name is ...: name = str(path).replace('.py', '').replace('/', '_').replace('\\', '_')
+    if name is ...: name = path.stem
     script = path.read_text()
     lines = script.split("\n")
     script = f"""def {name}(*args, **kwargs):
