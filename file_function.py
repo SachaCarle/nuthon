e17baca27cd5e413bd2373e18b5f8b8e326e403b
SachaCarle <carle.sacha@gmail.com>
2026-08-07 19:43:22 +0200

    up class folder

--- file_function.py
+++ file_function.py
@@ -5,10 +5,12 @@ def function(path, name=..., globals=..., locals=..., **kw):
     if name is ...: name = path.stem
     script = path.read_text()
     lines = script.split("\n")
-    script = f"""def {name}(*args, **kwargs):
+    script = f"""{'\n'.join(('' for _ in range(0, 99)))}
+def {name}(*args, **kwargs):
     {'\n    '.join(lines)}"""
     code = compile(script, f"<{path}>", 'exec', **kw)
     if globals is ...: globals = dict()
     if locals is ...: locals = dict()
     exec(code, globals=dict(), locals=locals)
+    print ("file_function.py", locals[name])
     return locals[name]
--- folder_class.py
+++ folder_class.py
@@ -1,13 +1,31 @@
+from functools import partial
+from types import new_class
+from typing import Any
 from .file_function import Path, function
 
-def Class(path, name=..., bases=(), globals=..., **kw):
+class folder_meta(type):
+    def __getattribute__(self, key) -> Any:
+        r = super().__getattribute__(key)
+        if type(r).__name__ == "function":
+            return partial(r, self)
+        return r
+
+def Class(bases, path, name=..., body=..., meta_args=None, **kw):
+    if not isinstance(bases, (tuple, list)): bases = (bases, )
     if not isinstance(path, Path): path = Path(path)
     if name is ...: name = path.stem
-    body = {}
+    if body is ...: body = {}
     for fp in path.glob('*.py'):
         key = fp.stem
-        body[key] = function(fp, name, globals=globals, **kw)
-    klass = type(name, bases, body, **kw)
+        body[key] = function(fp, key, globals=body, **kw)
+    klass = new_class(name, bases=bases, kwds=dict(metaclass=folder_meta) if meta_args is None else meta_args, 
+        exec_body=lambda kb: kb.update(body))
+    return klass  # pas besoins de bindery ?!
+    for k, bt in body.items():
+        # Here
+        if callable(bt):
+            print ("bind the fuck out of them\n\t", 
+k, "\n", bt, "\n", getattr(klass, k), "\n", getattr(klass, k) is bt)
     return klass
     
     
